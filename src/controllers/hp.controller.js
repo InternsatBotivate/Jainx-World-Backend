cc665e59c8bb945c3c7b2a4d381c0a272ce492fa
anuver11 <anuragver1999@gmail.com>
2026-02-13 16:51:58 +0530

    error log

--- src/controllers/hp.controller.js
+++ src/controllers/hp.controller.js
@@ -6,6 +6,6 @@ export const getPrograms = async (req, res) => {
     const data = await fetchCompensationData(req,res);
     res.json(data);
   } catch (error) {
-    res.status(500).json({ message: "Failed to fetch programs" });
+    res.status(500).json({ message: "Failed to fetch programs" , error });
   }
 };
