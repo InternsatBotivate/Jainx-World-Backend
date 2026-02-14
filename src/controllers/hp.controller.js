0f01249b898eb9fcb382c1b6e135a3c077a1f9ec
anuver11 <anuragver1999@gmail.com>
2026-02-14 10:41:47 +0530

    updateerert

--- src/controllers/hp.controller.js
+++ src/controllers/hp.controller.js
@@ -1,8 +1,10 @@
 import { fetchCompensationData } from "../services/hp.services.js";
 
 export const getPrograms = async (req, res) => {
+  console.log("Inside get program");
   try {
     const data = await fetchCompensationData(req.query);
+    console.log("Inside get program and got response");
     res.json(data);
   } catch (error) {
     console.error(error.response?.data || error.message);
--- src/services/hp.services.js
+++ src/services/hp.services.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 import { getToken, setToken } from "../utils/tokenManager.js";
 
-
-
-
 export const fetchHpToken = async () => {
+  console.log("inside fetch HP token")
   const existingToken = getToken();
-  if (existingToken) return existingToken;
-  
+  if (existingToken)  {
+    console.log("Inside Token exists")
+    return existingToken;
+  };
+
   const response = await axios.post(
     process.env.HP_TOKEN_URL,
     new URLSearchParams(),
@@ -23,13 +24,15 @@ export const fetchHpToken = async () => {
   );
 
   const { access_token, expires_in } = response.data;
-  
+
   setToken(access_token, expires_in);
+  console.log("inside fetch hp token token got and setup", access_token);
 
   return access_token;
 };
 
 export const fetchCompensationData = async (params) => {
+  console.log("Inside fetch composition data");
   const token = await fetchHpToken();
 
   const response = await axios.get(process.env.HP_PROGRAM_URL, {
@@ -39,6 +42,6 @@ export const fetchCompensationData = async (params) => {
       "x-api-key": process.env.HP_API_KEY,
     },
   });
-
+  console.log("Inside fetch composition data and got response data")
   return response.data;
 };
